1938d5d0a0d7961c06cd4ccf45e73d079909cad1
Ladzsz <londonmason602@gail.com>
2024-07-09 10:04:23 -0400

    made the function nesecary and obtained input from user

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ window.onload = function() {
     const insertBtn = document.querySelector('.insert-btn');
     insertBtn.addEventListener('click', function() {
         document.getElementById('formContainer').style.display = 'block';
-        
     });
 };
 
@@ -18,3 +17,41 @@ function Book(bookname, author, page) {
     this.author = author;
     this.page = page;
 }
+
+//function to add books to library
+
+function addbooktolibrary(library) {
+    const container = document.getElementById('card-container');
+    library.forEach(book => {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const bookName = document.createElement('h3');
+        bookName.textContent = `Book Name: ${book.bookName}`;
+        card.appendChild(bookName);
+
+        const author = document.createElement('h3');
+        author.textContent = `Author: ${book.author}`;
+        card.appendChild(author);
+
+        const pages = document.createElement('h3');
+        pages.textContent = `Pages: ${book.page}`;
+        card.appendChild(pages);
+
+        container.appendChild(card);
+    });
+}
+
+//getting user input from form
+
+document.getElementById('book-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const bookname = document.getElementById('bookname').value;
+    const author = document.getElementById('author').value;
+    const page = document.getElementById('pages').value;
+
+    const newBook = new Book(bookname, author, page);
+
+    library.push(newBook);
+});
